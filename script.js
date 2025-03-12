1e6800fbebb8b7be2f52a8c35a1a4a91a2394ae3
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-03-11 23:21:03 -0400

    Add function to flip the coin by generating a random number. Also add animation that it's only triggered when the coin or random button is clicked.

---
+++ script.js
@@ -0,0 +1,21 @@
+const coin = document.getElementById("coin-btn");
+const randomBtn = document.getElementById("random-btn");
+const resultSpan = document.querySelector(".flip-result");
+
+const flipCoin = () => {
+  // Add the flipping class to trigger the animation
+  coin.classList.add("flipping");
+
+  // Remove the flipping class after the animation ends
+  setTimeout(() => {
+    coin.classList.remove("flipping");
+
+    const result = Math.random < 0.5 ? "Heads" : "Tails";
+    resultSpan.textContent = result;
+
+    coin.innerText = result === "Heads" ? "$" : "|";
+  }, 1000); // Match the duration of the animation
+};
+
+coin.addEventListener("click", flipCoin);
+randomBtn.addEventListener("click", flipCoin);
